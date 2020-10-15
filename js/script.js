804a39e628927fc02e7ea1eb9b81a22664e705a0
v777999 <55867385+v777999@users.noreply.github.com>
2020-10-15 19:57:46 +0300

    Разметка Главной на гридах

---
+++ js/script.js
@@ -0,0 +1,25 @@
+console.log("Привет от JavaScript!");
+// Находим блок с кнопками
+const sliderCcontrol = document.querySelector(".slider-controls");
+console.log(sliderCcontrol);
+// Если нажата 1 и текущий не 1
+// у класса .page-body удалить текущий фон
+// и добавть ice cream для 1 слайда
+// у класса body background-color поменять на index1
+// удалть slide-current
+// slider-item добавить slide-current
+
+// Если нажата 2 и текущий не 2
+// у класса .page-body удалить текущий фон
+// и добавть ice cream для 2 слайда
+// у класса body background-color поменять на index2
+// удалть slide-current
+// slider-item добавить slide-current
+
+
+// Если нажата 3 и текущий не 3
+// у класса .page-body удалить текущий фон
+// и добавть ice cream для 3 слайда
+// у класса body background-color поменять на index3
+// удалть slide-current
+// slider-item добавить slide-current
