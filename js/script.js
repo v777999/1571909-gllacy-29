722ca4cf72a71cabbdf828d6c30e4f17a5fda6eb
v777999 <55867385+v777999@users.noreply.github.com>
2020-10-28 08:56:16 +0300

    Декоративные на главной

--- js/script.js
+++ js/script.js
@@ -1,25 +1,14 @@
-console.log("Привет от JavaScript!");
-// Находим блок с кнопками
-const sliderCcontrol = document.querySelector(".slider-controls");
-console.log(sliderCcontrol);
-// Если нажата 1 и текущий не 1
-// у класса .page-body удалить текущий фон
-// и добавть ice cream для 1 слайда
-// у класса body background-color поменять на index1
-// удалть slide-current
-// slider-item добавить slide-current
-
-// Если нажата 2 и текущий не 2
-// у класса .page-body удалить текущий фон
-// и добавть ice cream для 2 слайда
-// у класса body background-color поменять на index2
-// удалть slide-current
-// slider-item добавить slide-current
-
-
-// Если нажата 3 и текущий не 3
-// у класса .page-body удалить текущий фон
-// и добавть ice cream для 3 слайда
-// у класса body background-color поменять на index3
-// удалть slide-current
-// slider-item добавить slide-current
+ymaps.ready(init);
+function init() {
+  var myMap = new ymaps.Map(document.querySelector(".contact-info-map"), {
+    center: [59.939342, 30.329353],
+    zoom: 16
+  });
+  myPlacemark = new ymaps.Placemark([59.938635, 30.323118], {}, {
+    iconLayout: 'default#image',
+    iconImageHref: './img/map-pin.svg',
+    iconImageSize: [80, 140],
+    iconImageOffset: [-45, -145]
+  });
+  myMap.geoObjects.add(myPlacemark);
+};
